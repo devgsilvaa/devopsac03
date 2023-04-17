16abaa9a2153d16f2ad996d39b27997427c5192f
devgsilvaa <126527479+devgsilvaa@users.noreply.github.com>
2023-04-17 19:34:31 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,27 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+
+
+def exibir_numeros_primos():
+    qtd = 100
+    primos = [2]
+    num = 3
+
+    while len(primos) < 100:
+        for p in primos:
+            if num % p == 0:
+                break
+            else:
+                primos.append(num)
+
+            num += 2
+
+
+    for p in primos:
+        print(p)
+
+
+
